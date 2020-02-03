c1c219e903fe76ae6a4309609670f8e37d8a1e13
SimonWren <46319769+SimonWren@users.noreply.github.com>
2020-02-03 12:14:04 +0000

    pass logging instance to bolt connector and increase logging level to debug for function

--- DFC.ServiceTaxonomy.ApiFunction/Helpers/INeo4JHelper.cs
+++ DFC.ServiceTaxonomy.ApiFunction/Helpers/INeo4JHelper.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Neo4j.Driver;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace DFC.ServiceTaxonomy.ApiFunction.Helpers
 {
     public interface INeo4JHelper
     {
         Task<object> ExecuteCypherQueryInNeo4JAsync(string query,
-            IDictionary<string, object> statementParameters);
+            IDictionary<string, object> statementParameters, ILogger log);
 
         Task<IResultSummary> GetResultSummaryAsync();
     }
