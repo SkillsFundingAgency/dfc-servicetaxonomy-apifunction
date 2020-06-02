a3555062c2d44acf4180e73e164cc94978b7aa2a
olusola-adio <olusola_adio@hotmail.com>
2020-06-02 20:59:10 +0100

    more powershell changes

--- Resources/Scripts/Import-ApimOpenApiDefinitionFromFile.ps1
+++ Resources/Scripts/Import-ApimOpenApiDefinitionFromFile.ps1
@@ -44,19 +44,36 @@ Param(
 try {
     # --- Build context and retrieve apiid
     Write-Host "Building APIM context for $ApimResourceGroup\$InstanceName"
-    $Context = New-AzApiManagementContext -ResourceGroupName $ApimResourceGroup -ServiceName $InstanceName
+    $context = New-AzApiManagementContext -ResourceGroupName $ApimResourceGroup -ServiceName $InstanceName
 
-    # --- get a version set
-    Write-Host "Getting version set for $ApiPath"
-    $versionSets = Get-AzApiManagementApiVersionSet -Context $Context | Where-Object { $_.DisplayName -eq $ApiName }
-    if (!$versionSets) {
-        New-AzApiManagementApiVersionSet -Context $Context -Scheme Header -HeaderName "version" -Description $ApiName -Name $ApiName
+
+    # Check if it's already existing, if not a new versionset needs to be created, else use the existing one
+    # Version Set is unique by API, if there's an API with 2 versions there will be 1 version set with 2 APIs
+    Write-Host "[VERSION SET] Performing lookup. "
+    $versionSetLookup = Get-AzApiManagementApiVersionSet -Context $context | Where-Object { $_.DisplayName -eq "$ApiName" }  | Sort-Object -Property ApiVersionSetId -Descending | Select-Object -first 1
+    if($null -eq $versionSetLookup)
+    {
+        Write-Host "[VERSION SET] Version set NOT FOUND for: $ApiName, creating a new one. "
+        $versionSet = New-AzApiManagementApiVersionSet -Context $context -Name "$ApiName" -Scheme Header -HeaderName "version" -Description "$ApiName"
+        $versionSetId = $versionSet.Id
+        Write-Host "[VERSION SET] Created new version set, id: $versionSetId"
     }
-    $versionSet = Get-AzApiManagementApiVersionSet -Context $Context | Where-Object { $_.DisplayName -eq $ApiName }
+    else
+    {
+        Write-Host "[VERSION SET] Version set FOUND for: $ApiName, using existing one. "
+        $versionSetId = $versionSetLookup.ApiVersionSetId
+        Write-Host "[VERSION SET] Reusing existing versionset , id: $versionSetId"
+    }
+    
+    # import api from OpenAPI Specs
+    Write-Host  "[IMPORT] Importing OpenAPI: $openapiSpecs "
+    $api = Import-AzApiManagementApi -Context $context -SpecificationPath $OpenApiSpecificationFile -SpecificationFormat OpenApi -Path $ApiPath -ApiId "$ApiName$ApiVersion" -ApiVersion $ApiVersion -ApiVersionSetId $versionSetId -ErrorAction Stop -Verbose:$VerbosePreference
+    Write-Host  "[IMPORT] Imported API: $api.ApiId " 
+    
 
-    # --- Import openapi definition
-    Write-Host "Updating API $InstanceName\$($ApiName) from definition $($OutputFile.FullName)"
-    Import-AzApiManagementApi -Context $Context -SpecificationFormat OpenApi -SpecificationPath $OpenApiSpecificationFile -ApiId $ApiName -Path $ApiPath -ApiVersion $ApiVersion -ApiVersionSetId $versionSet.ApiVersionSetId -ErrorAction Stop -Verbose:$VerbosePreference
+    # # --- Import openapi definition
+    # Write-Host "Updating API $InstanceName\$($ApiName) from definition $($OutputFile.FullName)"
+    # Import-AzApiManagementApi -Context $Context -SpecificationFormat OpenApi -SpecificationPath $OpenApiSpecificationFile -ApiId $ApiName -Path $ApiPath -ApiVersion $ApiVersion -ApiVersionSetId $versionSet.ApiVersionSetId -ErrorAction Stop -Verbose:$VerbosePreference
 }
 catch {
    throw $_
